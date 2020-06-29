8f75c24ff5653673e413d858bdbfa58c68514b17
Ryan McCahon <mccahonr@yahoo.com>
2020-06-29 14:04:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,42 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
-  end
+  # write code here\
 
-  def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+  def initialize(s1, s2, s3)
+
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
       raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
-      end
     end
 
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if @s1 == @s2 && @s1 == @s3
+      return :equilateral
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+      return :isosceles
+    else
+      return :scalene
+    end
   end
 
 end
 
+
+
 class TriangleError < StandardError
-  # triangle error code
+  def message
+    "This is not a valid triangle."
+  end
 end
