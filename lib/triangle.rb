c34dbd6469423dd13f5bd90fdc9110b9a338d2a1
Ryan McCahon <mccahonr@yahoo.com>
2020-06-29 13:54:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid?
+
+  end
+
+  def kind
+    if @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b or @a == @c or @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
